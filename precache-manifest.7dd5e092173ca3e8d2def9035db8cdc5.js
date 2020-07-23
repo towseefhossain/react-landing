2ac7207758caec7103d5445b89838ff5398c704b
M Towseef Hossain <towseef.22aug@gmail.com>
2020-07-23 05:06:52 -0400

    Updates

---
+++ precache-manifest.7dd5e092173ca3e8d2def9035db8cdc5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394d493a3db8d76283320c97dd4079e3",
+    "url": "/react-landing/3eeeb336567113099595.worker.js"
+  },
+  {
+    "revision": "69cd844e5198ac97a0aad1207ced67af",
+    "url": "/react-landing/3eeeb336567113099595.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "e01271d9a3da5b22afb716019af3074c",
+    "url": "/react-landing/index.html"
+  },
+  {
+    "revision": "260dfb40e867fe9d5e81",
+    "url": "/react-landing/static/css/main.7836646e.chunk.css"
+  },
+  {
+    "revision": "19b001bb43987a3eb643",
+    "url": "/react-landing/static/js/2.dc520ab5.chunk.js"
+  },
+  {
+    "revision": "908678959397f2e04ac5f8f7afda467f",
+    "url": "/react-landing/static/js/2.dc520ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260dfb40e867fe9d5e81",
+    "url": "/react-landing/static/js/main.2107b8f1.chunk.js"
+  },
+  {
+    "revision": "4867443bdf1c18b9a5f1",
+    "url": "/react-landing/static/js/runtime-main.1b583fd5.js"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/react-landing/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/react-landing/static/media/map-image.f40ed5b1.png"
+  },
+  {
+    "revision": "6194aeafc42eb763d59053e67bc4be53",
+    "url": "/react-landing/static/media/rawkbrush-ywlym-webfont.6194aeaf.woff"
+  },
+  {
+    "revision": "a49d0840b3276aa5970edf71daa12755",
+    "url": "/react-landing/static/media/rawkbrush-ywlym-webfont.a49d0840.woff2"
+  }
+]);
