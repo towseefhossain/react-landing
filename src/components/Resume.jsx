4baec4b555e76678e40b9324c15ea091b900e9dd
M Towseef Hossain <towseef.22aug@gmail.com>
2020-07-26 03:33:13 -0400

    Removed zoom on mobile

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,25 +2,49 @@ import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf/dist/entry.webpack';
 const pdfFile = './resume.pdf'
 
-export default () => {
-    return (
-        <div className="page-section" id="resume">
-            <div className="container">
-                <div className="text-center">
-                    <h2 className="section-heading text-uppercase">Resume</h2>
-                    <h3 className="section-subheading text-muted">A quick run-down of what I've been up to</h3>
+class Resume extends Component {
+    constructor() {
+        super();
+        this.state = {
+            layoutMode: this.getLayoutMode()
+        }
+        this.onResize = this.onResize.bind(this)
+    }
+
+    onResize() {
+        this.setState({
+            layoutMode: this.getLayoutMode(),
+        });
+    }
+
+    getLayoutMode() {
+        return window.innerWidth > 1000 ? 'desktop' : 'mobile';
+    }
+
+    render() {
+        return (
+            <div className="page-section" id="resume">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-heading text-uppercase">Resume</h2>
+                        <h3 className="section-subheading text-muted">A quick run-down of what I've been up to</h3>
+                    </div>
+                    <div className="row text-center" style={{ justifyContent: 'center' }}>
+                        <Document
+                            file={pdfFile}
+                            width={this.state.layoutMode === 'desktop' ? "100pc" : (window.innerWidth * 0.7)}
+                        >
+                            <Page
+                                pageNumber={1}
+                                renderAnnotationLayer={false}
+                                renderTextLayer={false}
+                                width={this.state.layoutMode === 'desktop' ? "" : (window.innerWidth * 0.7)}></Page>
+                        </Document>
+                    </div>
                 </div>
-                <div className="row text-center" style={{ justifyContent: 'center' }}>
-                    <Document
-                        file={pdfFile}
-                    >
-                        <Page
-                            pageNumber={1}
-                            renderAnnotationLayer={false}
-                            renderTextLayer={false}></Page>
-                    </Document>
-                </div>
-            </div>
-        </div>
-    );
+            </div >
+        );
+    }
 }
+
+export default Resume
